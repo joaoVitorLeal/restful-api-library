e0ee7aaaca7b89c64e678c3743d849197135d15d
joaoVitorLeal <joaoleal98@outlook.com>
2025-03-29 03:43:15 -0300

    refactor: clean up and standardize code comments

--- libraryapi/src/main/java/io/github/joaoVitorLeal/libraryapi/config/DataSourceConfiguration.java
+++ libraryapi/src/main/java/io/github/joaoVitorLeal/libraryapi/config/DataSourceConfiguration.java
@@ -24,7 +24,7 @@ public class DataSourceConfiguration {
 
     @Bean
     public DataSource hikariDataSource() {
-        log.info("Initiating database connection at the URL: {}", url); // Log database connection attempt
+        log.info("Initiating database connection at the URL: {}", url);
 
         HikariConfig config = new HikariConfig();
         config.setUsername(username);
@@ -33,12 +33,12 @@ public class DataSourceConfiguration {
         config.setJdbcUrl(url);
 
         // Configures the connection pool properties for the data source
-        config.setMaximumPoolSize(10); // Maximum connections in the pool
-        config.setMinimumIdle(1); // Minimum idle connections in the pool
-        config.setPoolName("library-db-pool"); // Name of the pool
-        config.setMaxLifetime(6000000); // Connection lifetime in milliseconds
-        config.setConnectionTimeout(100000); // Timeout for establishing a connection
-        config.setConnectionTestQuery("select 1"); // Query to test connection health
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(6000000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("select 1");
 
         return new HikariDataSource(config);
     }
