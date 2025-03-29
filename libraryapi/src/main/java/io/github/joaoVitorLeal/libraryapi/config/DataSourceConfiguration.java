22e599857e866d54f96ace8ded7c5cc7e4fa3dba
joaoVitorLeal <joaoleal98@outlook.com>
2025-03-29 04:10:40 -0300

    git commit -m "chore(gitignore): exclude .env and cache directories"

--- libraryapi/src/main/java/io/github/joaoVitorLeal/libraryapi/config/DataSourceConfiguration.java
+++ libraryapi/src/main/java/io/github/joaoVitorLeal/libraryapi/config/DataSourceConfiguration.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
