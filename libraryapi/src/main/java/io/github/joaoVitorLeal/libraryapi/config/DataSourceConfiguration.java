970f39ac1edc06d171e4bd20fcc1428d7eca6858
joaoVitorLeal <joaoleal98@outlook.com>
2025-03-29 00:17:06 -0300

    add:     add: Dockerfile for building the application image     add: logging configuration to ExceptionHandle and User classes
    build:
        build: add Swagger and Spring Actuator dependencies
    
    docs:
        docs: implement API documentation using Swagger
    
    TODO's:
        feat: start docker image upload process
        chore: remove unnecessary comments and add relevant comments in classes

--- libraryapi/src/main/java/io/github/joaoVitorLeal/libraryapi/config/DataSourceConfiguration.java
+++ libraryapi/src/main/java/io/github/joaoVitorLeal/libraryapi/config/DataSourceConfiguration.java
@@ -2,15 +2,14 @@ package io.github.joaoVitorLeal.libraryapi.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@Slf4j
 public class DataSourceConfiguration {
 
     @Value("${spring.datasource.url}")
@@ -22,23 +21,46 @@ public class DataSourceConfiguration {
     @Value("${spring.datasource.driver-class-name}")
     String driver;
 
-    @Bean
-    public DataSource hikariDataSource() {
-        log.info("Initiating database connection at the URL: {}", url);
+    /**
+     * Criação de um data source básico.
+     * */
+//    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource(); // Não utiliza-se em produção
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
 
+        return ds;
+    }
+
+    /**
+     * Por PADRÃO o Spring Boot ja usa o Hikari
+     * Conexão com o Hikari data source,
+     * é utilizado em produção por possuir melhor desempenho,
+     * maior complexidade e gestão dinâmico de pool de conexões.
+     * Repositório Hikari: https://github.com/brettwooldridge/HikariCP
+     *
+     * @return config
+     */
+    @Bean  // Configurando Data Source
+    public DataSource hikariDataSource() {
         HikariConfig config = new HikariConfig();
         config.setUsername(username);
         config.setPassword(password);
         config.setDriverClassName(driver);
         config.setJdbcUrl(url);
 
-        // Configures the connection pool properties for the data source
-        config.setMaximumPoolSize(10);
-        config.setMinimumIdle(1);
-        config.setPoolName("library-db-pool");
-        config.setMaxLifetime(6000000);
-        config.setConnectionTimeout(100000);
-        config.setConnectionTestQuery("select 1");
+        /*
+         * Configurando as propriedades do pool de conexões do Data Source.
+         * */
+        config.setMaximumPoolSize(10); // Tamanho máximo de conexões no pool - (10)
+        config.setMinimumIdle(1); // Tamanho mínimo inicial do pool - (1)
+        config.setPoolName("library-db-pool"); // Nome do pool
+        config.setMaxLifetime(6000000); // Duração da conexão em milissegundos // 600 mil ms (10 minutos)
+        config.setConnectionTimeout(100000); // Timeout para estabelecer uma conexão
+        config.setConnectionTestQuery("select 1"); // Query de teste de conexão
 
         return new HikariDataSource(config);
     }
